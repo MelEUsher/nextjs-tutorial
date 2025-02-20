f734c218cd24f682ffe8e428e4734588b74bb55e
Melissa Usher <MelEUsher17@gmail.com>
2025-02-19 21:29:34 -0600

    user response

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users =  await res.json ();
 
   return <div>UsersPage</div>;
 };
