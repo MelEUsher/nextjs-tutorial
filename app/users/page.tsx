805a255c753d442fd041283059c771c336931293
Melissa Usher <MelEUsher17@gmail.com>
2025-02-19 21:41:00 -0600

    added user list map id and name

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 
+interface User {
+  id: number;
+  name: string;
+}
+
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await res.json();
+  const users: User [] = await res.json();
 
-  return <div>UsersPage</div>;
+  return 
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)       
+        }
+      </ul>
+    </>
 };
 
 export default UsersPage;
